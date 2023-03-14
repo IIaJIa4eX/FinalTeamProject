4a2232a0d2c3d35aad766c78271952a596f44f13
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2023-03-14 22:15:54 +0300

    Added DatabaseConnector Project

---
+++ FinalProject/DatabaseConnector/Interfaces/IMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace DatabaseConnector.Interfaces
+{
+    public interface IMessage
+    {
+        int Id { get;  }
+        int UserId { get;  }
+        Content Content { get;  }
+        int Rating { get;  }
+        bool IsVisible { get; }
+        DateTime CreationDate { get; }
+    }
+}
