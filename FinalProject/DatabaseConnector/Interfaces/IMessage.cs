f2b41a8ba190022857aa48c3fe98cadff6a1e3ba
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2023-03-15 18:46:32 +0300

    Connection strings added

--- FinalProject/DatabaseConnector/Interfaces/IMessage.cs
+++ FinalProject/DatabaseConnector/Interfaces/IMessage.cs
@@ -5,7 +5,7 @@
         int Id { get;  }
         int UserId { get;  }
         Content Content { get;  }
-        int Rating { get;  }
+        // int Rating { get;  }
         bool IsVisible { get; }
         DateTime CreationDate { get; }
     }
---
+++ FinalProject/DatabaseConnector/User.cs
@@ -0,0 +1,14 @@
+﻿namespace DatabaseConnector;
+
+public class User
+{
+    public string Pseudo { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Patronimic { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public short Role { get; set; }
+    public DateTime BirthDate { get; set; }
+    public bool IsBanned { get;set; }
+}
