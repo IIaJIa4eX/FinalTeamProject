f93d1dbd320a1d9992a03eb2a5d245e0e44aa561
Bzic111 <77696205+Bzic111@users.noreply.github.com>
2023-03-15 19:15:36 +0300

    Ivan working (#11)
    * Added DatabaseConnector Project
    
    * Промежуточный результат
    
    * Connection strings added
    
    * Added DatabaseConnector Project
    
    * Промежуточный результат
    
    * Connection strings added
    
    * Added DatabaseConnector Project
    
    * Промежуточный результат
    
    * Connection strings added
    
    * Added DatabaseConnector Project
    
    * Connection strings added

---
+++ FinalProject/DatabaseConnector/Context.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseConnector;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Extensions;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace DatabaseConnector;
+
+public class Context : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Issue> Issues { get; set; }
+    public DbSet<SessionInfo> SessionInfos { get; set; }
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
