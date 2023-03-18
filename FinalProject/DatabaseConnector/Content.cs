e1109039714b0b4054faf0532fd4a2973d378d35
Bzic111 <bzic111@yandex.ru>
2023-03-18 12:36:18 +0300

    Изменение в файле Content.cs

--- FinalProject/DatabaseConnector/Content.cs
+++ FinalProject/DatabaseConnector/Content.cs
@@ -15,8 +15,9 @@ public class Content
     [Column]
     public bool IsVisible { get; set; }
 
-    public virtual User? Users { get; set; }
+    [ForeignKey(nameof(User))]
+    public Guid UserId { get; set; }
 
-    /*[Column]
-    public string? Text { get; set; }*/
+    [Column]
+    public string? Text { get; set; }
 }
