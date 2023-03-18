340820ca93a9dd1fda9972d651f4d401976f6bf3
Bzic111 <bzic111@yandex.ru>
2023-03-18 12:23:19 +0300

    тесты

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -44,12 +44,6 @@ public class Program
             // builder.Services.AddControllersWithViews();
 
         
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-        }
-        app.UseStaticFiles();
-
             using (var fs = new FileStream("dbcstring.json", FileMode.Open))
             {
 
