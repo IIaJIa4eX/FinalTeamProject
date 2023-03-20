37b51bd7746cf7d347f75241f8e0e6158559c35f
Bzic111 <bzic111@yandex.ru>
2023-03-20 10:15:38 +0300

    Исправлена система подключения к БД

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -16,76 +16,35 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
-        Database db = null;
-        string connection = "";
+        builder.Services.AddHttpLogging(logging =>
+        {
+            logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+            logging.RequestBodyLogLimit = 4096;
+            logging.ResponseBodyLogLimit = 4096;
+            logging.RequestHeaders.Add("Authorization");
+            logging.RequestHeaders.Add("X-Real-IP");
+            logging.RequestHeaders.Add("X-Forwared-For");
+        });
+        builder.Host.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+        }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
         if (File.Exists("dbcstring.json"))
         {
-            //builder.Services.AddDbContext<FinalProjectDbContext>(options =>
-            //{
-            //    options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
-            //});
-            builder.Services.AddHttpLogging(logging =>
-            {
-                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
-                logging.RequestBodyLogLimit = 4096;
-                logging.ResponseBodyLogLimit = 4096;
-                logging.RequestHeaders.Add("Authorization");
-                logging.RequestHeaders.Add("X-Real-IP");
-                logging.RequestHeaders.Add("X-Forwared-For");
-            });
-            builder.Host.ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-                logging.AddConsole();
-            }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
-
-
-            // builder.Services.AddControllersWithViews();
-
-
             using (var fs = new FileStream("dbcstring.json", FileMode.Open))
             {
-
-                db = JsonSerializer.Deserialize<Database>(fs)!;
-                {
-                }
-                /*
-                {
-                  "Name": "Postgre",
-                  "UserName": "Ivan",
-                  "ConnectionString": "Host=localhost;Port=5432;Database=TheForumDB;Username=postgres;Password=393318156a"
-                }
-                {
-                  "Name": "MySQL",
-                  "UserName": "Ivan",
-                  "ConnectionString": "Server=localhost;Port=3306;Database=FinalProjectDatabase;Uid=bzic;Pwd=393318156a404056792b;"
-                }
-                */
-
+                string connection = JsonSerializer.Deserialize<Database>(fs)!.ConnectionString;
+                builder.Services.AddDbContext<Context>(options => options.UseNpgsql(connection));
             }
         }
-
-        switch (db.Name)
+        else
         {
-            case "Postgre":
-                connection = db.ConnectionString;
-                builder.Services.AddDbContext<Context>(options => options.UseNpgsql(connection));
-                break;
-            case "MySQL":
-                connection = db.ConnectionString;
-                builder.Services.AddDbContext<Context>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 28))));
-                break;
-
-            case "MSSQL":
-                connection = db.ConnectionString;
-                builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connection));
-                break;
-
-            default:
-                break;
+            throw new FileLoadException("dbcstring not exist, can`t find connection string for database!");
         }
 
-        
+        builder.Services.AddScoped<EFGenericRepository<User>>();
         builder.Services.AddScoped<EFGenericRepository<Comment>>();
         builder.Services.AddScoped<EFGenericRepository<Post>>();
         builder.Services.AddScoped<EFGenericRepository<Issue>>();
