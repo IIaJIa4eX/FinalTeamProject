4ead375ebb922d3a77f6c5c6ab1a452618f54dec
Bzic111 <bzic111@yandex.ru>
2023-03-20 09:47:16 +0300

    изменён файд Database.cs и схема подключения к БД. Теперь нужна только строка connectonstring в файле dbcstring.json

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -1,12 +1,9 @@
-using Microsoft.AspNetCore.HttpLogging;
-using NLog.Web;
 using DatabaseConnector;
-using DatabaseConnector.Interfaces;
-//using DatabaseConnector.Migrations;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System.Text.Json;
 using FinalProject.DataBaseContext;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using NLog.Web;
+using System.Text.Json;
 
 namespace FinalProject;
 
@@ -16,76 +13,40 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
-        Database db = null;
         string connection = "";
+        builder.Services.AddHttpLogging(logging =>
+        {
+            logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+            logging.RequestBodyLogLimit = 4096;
+            logging.ResponseBodyLogLimit = 4096;
+            logging.RequestHeaders.Add("Authorization");
+            logging.RequestHeaders.Add("X-Real-IP");
+            logging.RequestHeaders.Add("X-Forwared-For");
+        });
+        builder.Host.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+        }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
+
+        // builder.Services.AddControllersWithViews();
+
+
         if (File.Exists("dbcstring.json"))
         {
-            //builder.Services.AddDbContext<FinalProjectDbContext>(options =>
-            //{
-            //    options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
-            //});
-            builder.Services.AddHttpLogging(logging =>
-            {
-                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
-                logging.RequestBodyLogLimit = 4096;
-                logging.ResponseBodyLogLimit = 4096;
-                logging.RequestHeaders.Add("Authorization");
-                logging.RequestHeaders.Add("X-Real-IP");
-                logging.RequestHeaders.Add("X-Forwared-For");
-            });
-            builder.Host.ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-                logging.AddConsole();
-            }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
-
-
-            // builder.Services.AddControllersWithViews();
-
-
             using (var fs = new FileStream("dbcstring.json", FileMode.Open))
             {
-
-                db = JsonSerializer.Deserialize<Database>(fs)!;
-                {
-                }
-                /*
-                {
-                  "Name": "Postgre",
-                  "UserName": "Ivan",
-                  "ConnectionString": "Host=localhost;Port=5432;Database=TheForumDB;Username=postgres;Password=393318156a"
-                }
-                {
-                  "Name": "MySQL",
-                  "UserName": "Ivan",
-                  "ConnectionString": "Server=localhost;Port=3306;Database=FinalProjectDatabase;Uid=bzic;Pwd=393318156a404056792b;"
-                }
-                */
-
+                connection = JsonSerializer.Deserialize<Database>(fs)!.ConnectionString;
+                builder.Services.AddDbContext<Context>(options => options.UseNpgsql(connection));
             }
         }
-
-        switch (db.Name)
+        else
         {
-            case "Postgre":
-                connection = db.ConnectionString;
-                builder.Services.AddDbContext<Context>(options => options.UseNpgsql(connection));
-                break;
-            case "MySQL":
-                connection = db.ConnectionString;
-                builder.Services.AddDbContext<Context>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 28))));
-                break;
-
-            case "MSSQL":
-                connection = db.ConnectionString;
-                builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connection));
-                break;
-
-            default:
-                break;
+            throw new FileLoadException("dbcstring.json not exist, no connection string for database!");
         }
 
-        
+        builder.Services.AddScoped<EFGenericRepository<User>>();
         builder.Services.AddScoped<EFGenericRepository<Comment>>();
         builder.Services.AddScoped<EFGenericRepository<Post>>();
         builder.Services.AddScoped<EFGenericRepository<Issue>>();
