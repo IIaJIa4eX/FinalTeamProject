4299afd5880e7bdf667e6389dd8c90ee75ba946c
Bzic111 <bzic111@yandex.ru>
2023-03-20 10:15:38 +0300

    Исправлена система подключения к БД

--- FinalProject/DatabaseConnector/Database.cs
+++ FinalProject/DatabaseConnector/Database.cs
@@ -5,12 +5,6 @@ namespace DatabaseConnector;
 
 public class Database
 {
-    [JsonPropertyName("Name")]
-    public string Name { get; set; }
-
-    [JsonPropertyName("UserName")]
-    public string UserName { get; set; }
-    
     [JsonPropertyName("ConnectionString")]
     public string ConnectionString { get; set; }
     public Database()
