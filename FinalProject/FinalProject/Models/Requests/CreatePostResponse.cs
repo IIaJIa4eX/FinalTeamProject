9d07df867ac3501e123853f09321e4c1c56c018e
Bzic111 <bzic111@yandex.ru>
2023-03-21 20:40:09 +0300

    deleting Request and DTO

--- FinalProject/FinalProject/Models/Requests/CreatePostResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinalProject.Models.Requests
-{
-    public class CreatePostResponse : IOperationResult
-    {
-        public string? PostId { get; set; }
-        public int ErrorCode { get; set; }
-        public string? ErrorMessage { get; set; }
-    }
-}
