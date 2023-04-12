b821b1659a4e7e48b707c991cc1eff5216258423
Bzic111 <bzic111@yandex.ru>
2023-04-12 17:27:42 +0300

    Кликабилити пост

--- FinalProject/FinalProject/Controllers/PostController.cs
+++ FinalProject/FinalProject/Controllers/PostController.cs
@@ -25,54 +25,8 @@ public class PostController : Controller
     [AllowAnonymous]
     public IActionResult Index([FromRoute] int id)
     {
-        //var post = _postDataHandler.GetById(id);
-        Post post = new Post()
-        {
-            CreationDate = DateTime.Now,
-            Id = id,
-            ContentId = 1,
-            Rating = 1,
-            IsVisible = true,
-            Category = "Cat",
-            User = new User()
-            {
-                Id = 1,
-                FirstName = "fName",
-                LastName = "lName",
-                NickName = "nName",
-                Birthday = DateTime.Now,
-                Email = "asd@asd.as",
-                PasswordHash = "qwer",
-                PasswordSalt = "asdf",
-                IsBanned = false,
-                UserRole = "a",
-                Patronymic = "patronymic"
-            },
-            Content = new Content()
-            {
-                Text = "Content",
-                Id = 1,
-                CreationDate = DateTime.Now,
-                IsVisible = true
-            },
-            Comments = new List<Comment>(),
-            UserId = 1
-        };
-        post.Comments.Add(new Comment()
-        {
-            Content = new Content()
-            {
-                Id = 1,
-                CreationDate = DateTime.Now,
-                IsVisible = true,
-                Text = "Visible text"
-            },
-            CreationDate = DateTime.Now,
-            IsVisible = true,
-            Id = 1,
-            UserId = 1
-        });
-        //return Ok($"{post.CreationDate}, {post.ContentId}, {post.User.NickName}");
+        var post = _postDataHandler.GetById(id);
+
         return View(post);
     }
 
